cca819d25b5d23637345149daec96e0a8510f372
rackhuber <rackhuber@my.rackhub.net>
2012-03-19 18:26:43 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rackathon::Application.config.secret_token = '2786e9c8edd79c3ddd0094e83c80a776908b7130fa42b2be1ba77bc87ee5772902c8194f75bc446a67a225077f39331a7de03fa5044d90819a7aa23ebdd31bf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rackathon::Application.config.session_store :cookie_store, key: '_rackathon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rackathon::Application.config.session_store :active_record_store
